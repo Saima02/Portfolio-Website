e83b533e3e71d884cdaf85d69f99b8935ca61eaa
Saima02 <ansarisaimaa02@gmail.com>
2026-01-06 16:33:21 +0530

    Headline update

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -27,7 +27,7 @@ const social: Social = [
   {
     name: "GitHub",
     icon: "github",
-    link: "https://github.com/Saima02",
+    link: "https://github.com/Saima-Ansari2",
   },
   {
     name: "LinkedIn",
@@ -47,7 +47,7 @@ const home: Home = {
   label: "Home",
   title: `${person.name}`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
-  headline: <>Building and learning through the world of web technology.</>,
+  headline: <>Analyzing data. Learning daily. Growing constantly.</>,
   featured: {
     display: false,
     title: (
